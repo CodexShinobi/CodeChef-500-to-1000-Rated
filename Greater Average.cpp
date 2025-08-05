ff3c9db128ddada71f7fbde424714b7ab70ba451
Navneet Prakash <142720492+CodexShinobi@users.noreply.github.com>
2025-08-05 14:46:46 +0530

    Create Greater Average.cpp

---
+++ Greater Average.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	double a,b,c;
+	cin>>a>>b>>c;
+	double ans=(a+b)/2.0;
+	
+	if(ans>c)
+	cout<<"yes"<<endl;
+	else
+	cout<<"no"<<endl;
+	}
+return 0;
+}
