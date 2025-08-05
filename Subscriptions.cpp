5fd159b58add1c519099f7de4faa46c8ade03084
Navneet Prakash <142720492+CodexShinobi@users.noreply.github.com>
2025-08-05 14:47:37 +0530

    Create Subscriptions.cpp

---
+++ Subscriptions.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	// your code goes here
+	int t ;
+	cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	   int subs=(n+5)/6;
+	       cout<<subs*x<<endl;
+	       
+	    }
+	    
+}
+
