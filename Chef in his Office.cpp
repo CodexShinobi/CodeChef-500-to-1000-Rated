5702a306d79e1e61d504d515f5b59bb7a0901f94
Navneet Prakash <142720492+CodexShinobi@users.noreply.github.com>
2025-08-06 21:13:39 +0530

    Create Chef in his Office.cpp

---
+++ Chef in his Office.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;  // Number of test cases
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+        int total_hours = 4 * X + Y;
+        cout << total_hours << endl;
+    }
+
+    return 0;
+}
