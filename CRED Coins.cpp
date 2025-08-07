c82c7afbff2bd8a772e4be2f61983622732e35f5
Navneet Prakash <142720492+CodexShinobi@users.noreply.github.com>
2025-08-07 11:17:19 +0530

    Create CRED Coins.cpp

---
+++ CRED Coins.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+        int coins = X * Y;
+        int bags = coins / 100;
+        cout << bags << endl;
+    }
+    return 0;
+}
