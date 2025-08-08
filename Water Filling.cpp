2bb7717b1f09157f82890837627aa1f55e29d140
Navneet Prakash <142720492+CodexShinobi@users.noreply.github.com>
2025-08-08 14:29:52 +0530

    Create Water Filling.cpp

---
+++ Water Filling.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int B1, B2, B3;
+        cin >> B1 >> B2 >> B3;
+
+        int emptyCount = (B1 == 0) + (B2 == 0) + (B3 == 0);
+
+        if (emptyCount >= 2)
+            cout << "Water filling time\n";
+        else
+            cout << "Not now\n";
+    }
+    return 0;
+}
